56f4b066c49d763a4fda50cded30abc3fd2df9ac
Bruno Alves da Silva <contato@alvesbruno.com>
2017-03-27 08:20:19 -0300

    Criação das tabelas migration

---
+++ database/migrations/2017_03_27_105634_criar_tabela_pessoa.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriarTabelaPessoa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pessoa', function (Blueprint $table) {
+            
+            // Básico
+            $table->increments('cd_pessoa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pessoa');
+    }
+}
