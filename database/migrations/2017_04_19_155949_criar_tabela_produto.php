dfee90e36a6f6ffe1fbd6ebbad762fe6bda435a0
Willian Barreto <willianblopes@gmail.com>
2017-04-19 13:16:58 -0300

    Criado migration Produto Close #31

---
+++ database/migrations/2017_04_19_155949_criar_tabela_produto.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriarTabelaProduto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+     		chema::create('produto', function (Blueprint $table) {
+            
+            // Básico
+            $table->increments('cd_produto');
+            $table->string('nm_produto', 50);
+			$table->string('ds_produto'200);
+			$table->float('vl_produto'8,2);
+			$table->binary('im_produto');
+			$table->integer('qt_minima_produto',5);
+			$table->integer('qt_maxima_produto',5);
+			$table->integer('qt_estoque_produto',5);
+            $table->timestamps();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto');
+    }
+}
