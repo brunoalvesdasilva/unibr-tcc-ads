e65767a00f0b8940ece247563131b848f91c5190
Victor Hugo Costa <victor.hlscosta@gmail.com>
2017-05-24 21:54:08 -0300

    Criação das Views de estoque, chamado, servico e usuario

--- resources/views/usuario/list.blade.php
+++ resources/views/usuario/list.blade.php
@@ -1,15 +1,15 @@
 @extends('layout/public')
 @section('content')
     <div class="component-title">
-        <h1>Listagem do estoque</h1>
+        <h1>Listagem de usuarios</h1>
     </div>
         
     <div class="component-barra-menu">
         <div class="btn-group pull-right" role="group">
-            <a href="/estoque/help" class="btn btn-default">Ajuda</a>
-            <a href="/estoque/pdf" class="btn btn-default">PDF</a>
-            <a href="/estoque" class="btn btn-default">Atualizar</a>
-            <a href="/estoque/create" class="btn btn-default">Novo Registro</a>
+            <a href="/usuario/help" class="btn btn-default">Ajuda</a>
+            <a href="/usuario/pdf" class="btn btn-default">PDF</a>
+            <a href="/usuario" class="btn btn-default">Atualizar</a>
+            <a href="/usuario/create" class="btn btn-default">Novo Registro</a>
         </div>
     </div>
         
