ba7e7865a5bd6f84581d5a77de0772db8424a014
Victor Hugo Costa <victor.hlscosta@gmail.com>
2017-05-29 21:31:26 -0300

    Versão final feita em aula dia 29/05

--- resources/views/conta/edit.blade.php
+++ resources/views/conta/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layout/public')
 @section('content')
     <div class="component-title">
-        <h1>Cadastro de contas</h1>
+        <h1>Edição de conta</h1>
     </div>
 
     <div class="component-barra-menu">
@@ -23,29 +23,29 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <form class="form-horizontal" action="/conta" method="POST">
+                <form class="form-horizontal" action="/conta/{{$conta->cd_conta}}" method="POST">
                     <div class="form-group">
                         <label for="nm_conta" class="col-md-4 control-label">Nome :</label>
                         <div class="col-md-6">
-                            <input type="text" class="form-control" id="nm_conta" name="nm_conta" placeholder="Nome da conta"/>
+                            <input type="text" class="form-control" id="nm_conta" name="nm_conta" placeholder="Nome da conta" value="{{$conta->nm_conta}}"/>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="cd_agencia_conta" class="col-md-4 control-label">Agência :</label>
                         <div class="col-md-6">
-                            <input type="number" class="form-control" id="cd_agencia_conta" name="cd_agencia_conta" placeholder="Nº da agência"/>
+                            <input type="number" class="form-control" id="cd_agencia_conta" name="cd_agencia_conta" placeholder="Nº da agência" value="{{$conta->cd_agencia_conta}}" />
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="cd_numero_conta" class="col-md-4 control-label">Conta :</label>
                         <div class="col-md-6">
-                            <input type="number" class="form-control" id="cd_numero_conta" name="cd_numero_conta" placeholder="Nº da conta"/>
+                            <input type="number" class="form-control" id="cd_numero_conta" name="cd_numero_conta" placeholder="Nº da conta" value="{{$conta->cd_numero_conta}}" />
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="vl_inicial_conta" class="col-md-4 control-label">Valor Inicial:</label>
                         <div class="col-md-6">
-                            <input type="text" class="form-control" id="vl_inicial_conta" name="vl_inicial_conta" placeholder="Valor inicial R$" />
+                            <input type="text" class="form-control" id="vl_inicial_conta" name="vl_inicial_conta" placeholder="Valor inicial R$" value="{{$conta->vl_inicial_conta}}" />
                         </div>
                     </div>
                     <div class="form-group">
@@ -53,41 +53,41 @@
                         <div class="col-md-6">
                             <div class="radio">
                                 <label>
-                                    <input type="radio" id="nm_tipo_conta0" name="nm_tipo_conta" value="poupanca"> Poupança
+                                    <input type="radio" id="nm_tipo_conta0" name="nm_tipo_conta" value="poupanca" {{$conta->nm_tipo_conta=="poupanca"?"checked":""}}> Poupança
                                 </label>
                             </div>
                             <div class="radio">
                                 <label>
-                                    <input type="radio"  id="nm_tipo_conta1" name="nm_tipo_conta" value="corrente"> Corrente
+                                    <input type="radio"  id="nm_tipo_conta1" name="nm_tipo_conta" value="corrente" {{$conta->nm_tipo_conta=="corrente"?"checked":""}}> Corrente
                                 </label>
                             </div>
                             <div class="radio">
                                 <label>
-                                    <input type="radio"  id="nm_tipo_conta2" name="nm_tipo_conta" value="cartao"> Cartão
+                                    <input type="radio"  id="nm_tipo_conta2" name="nm_tipo_conta" value="cartao" {{$conta->nm_tipo_conta=="cartao"?"checked":""}}> Cartão
                                 </label>
                             </div>
                             <div class="radio">
                                 <label>
-                                    <input type="radio"  id="nm_tipo_conta3" name="nm_tipo_conta" value="investimento"> Investimento
+                                    <input type="radio"  id="nm_tipo_conta3" name="nm_tipo_conta" value="investimento" {{$conta->nm_tipo_conta=="investimento"?"checked":""}}> Investimento
                                 </label>
                             </div>
                             <div class="radio">
                                 <label>
-                                    <input type="radio"  id="nm_tipo_conta4" name="nm_tipo_conta" value="caixa"> Caixa
+                                    <input type="radio"  id="nm_tipo_conta4" name="nm_tipo_conta" value="caixa" {{$conta->nm_tipo_conta=="caixa"?"checked":""}}> Caixa
                                 </label>
                             </div>
                             <div class="radio">
                                 <label>
-                                    <input type="radio"  id="nm_tipo_conta5" name="nm_tipo_conta" value="outras"> Outras
+                                    <input type="radio"  id="nm_tipo_conta5" name="nm_tipo_conta" value="outras" {{$conta->nm_tipo_conta=="outras"?"checked":""}}> Outras
                                 </label>
                             </div>
                         </div>
                     </div>
                     <div class="form-group">
                         <div class="col-md-offset-4 col-md-6">
-                            <button type="submit" class="btn btn-default">Cadastrar</button>
+                            <button type="submit" class="btn btn-info">Editar</button>
                             {{ csrf_field() }}
-                            {{ method_field('POST') }}
+                            {{ method_field('PUT') }}
                         </div>
                     </div>
                 </form>
--- resources/views/conta/list.blade.php
+++ resources/views/conta/list.blade.php
@@ -1,61 +1,61 @@
 @extends('layout/public')
 @section('content')
-    <div class="component-title">
+    <div class="component-title" data-intro='Nessa tela você poderá criar, editar e excluir os registros de contas cadastrados no sistema.'>
         <h1>Listagem de contas</h1>
     </div>
-        
+
     <div class="component-barra-menu">
         <div class="btn-group pull-right" role="group">
-            <a href="/conta/help" class="btn btn-default">Ajuda</a>
-            <a href="/conta/pdf" class="btn btn-default">PDF</a>
-            <a href="/conta" class="btn btn-default">Atualizar</a>
-            <a href="/conta/create" class="btn btn-default">Novo Registro</a>
+            <a href="#/conta/help" class="btn btn-default btn-help" data-intro='Clique aqui para ter uma ajuda igual a essa na página'>Ajuda</a>
+            <a href="/conta/pdf" class="btn btn-default" data-intro='Clique aqui para baixar um relatório da página atual em PDF'>PDF</a>
+            <a href="/conta" class="btn btn-default" data-intro='Clique aqui para atualizar a tela'>Atualizar</a>
+            <a href="/conta/create" class="btn btn-default" data-intro='Clique aqui para adicionar um novo registro'>Novo Registro</a>
         </div>
     </div>
-        
+
+    @if(Session::has('message'))
+        <div class="alert alert-success">
+            {{ Session::get('message') }}
+        </div>
+    @endif
+
     <table class="table table-striped table-hovered table-condesend">
         <thead>
-            <tr>
-                <th>#</th>
-                <th>Nome</th>
-                <th>Tipo</th>
-                <th>Valor do contrato</th>
-                <th>Ação</th>
-            </tr>
+        <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>Tipo</th>
+            <th>Valor do contrato</th>
+            <th>Ação</th>
+        </tr>
         </thead>
         <tbody>
+        @forelse($listaContas as $conta)
             <tr>
-                <td>#</td>
-                <td>Nome</td>
-                <td>Poupança</td>
-                <td>$</td>
+                <td>{{$conta->cd_conta}}</td>
+                <td>{{$conta->nm_conta}}</td>
+                <td>{{$conta->nm_tipo_conta}}</td>
+                <td>{{$conta->vl_atual_conta}}</td>
                 <td>
                     <div class="btn-group">
+                        <a href="/conta/{{$conta->cd_conta}}" type="button" class="btn btn-default">Ver</a>
                         <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Ver <span class="caret"></span>
+                            <span class="caret"></span>
+                            <span class="sr-only">Toggle Dropdown</span>
                         </button>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Excluir</a></li>
+                            <li><a href="/conta/{{$conta->cd_conta}}/edit">Editar</a></li>
                         </ul>
                     </div>
                 </td>
             </tr>
+        @empty
             <tr>
-                <td>#</td>
-                <td>Nome</td>
-                <td>Poupança</td>
-                <td>$</td>
-                <td>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Ver <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Excluir</a></li>
-                        </ul>
+                <td colspan="5" class="aling-center">
+                    Não há contas cadastradas
                     </div>
-                </td>
             </tr>
+        @endforelse
         </tbody>
     </table>
 @stop
