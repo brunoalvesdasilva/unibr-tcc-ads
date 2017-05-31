670149e4d6455fb81e80d398d3dbd88d33245825
Willian Barreto <willianblopes@gmail.com>
2017-05-30 22:54:22 -0300

    Criado Models Close #49

---
+++ app/Http/Controllers/Log.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Log extends Model
+{
+    // Nome da tabela
+    protected $table = 'log';
+    
+    // Chave primário
+    protected $primaryKey = 'cd_log';
+    
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
