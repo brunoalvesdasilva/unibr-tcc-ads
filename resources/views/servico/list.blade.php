ddf82ae634b931f06baae220c854c36b8213d7ab
Victor Hugo Costa <victor.hlscosta@gmail.com>
2017-05-31 23:42:05 -0300

    Ajustes na tela de serviços

--- resources/views/servico/list.blade.php
+++ resources/views/servico/list.blade.php
@@ -1,19 +1,25 @@
 @extends('layout/public')
 @section('content')
-    <div class="component-title">
+    <div class="component-title" data-intro='Nessa tela você irá manusear as informações dos serviços da empresa.'>
         <h1>Listagem de serviços</h1>
     </div>
         
     <div class="component-barra-menu">
         <div class="btn-group pull-right" role="group">
-            <a href="/servico/help" class="btn btn-default">Ajuda</a>
-            <a href="/servico/pdf" class="btn btn-default">PDF</a>
-            <a href="/servico" class="btn btn-default">Atualizar</a>
-            <a href="/servico/create" class="btn btn-default">Novo Registro</a>
+            <a href="#/servico/help" class="btn btn-default btn-help" data-intro='Clique aqui para ter uma ajuda igual a essa na página.'>Ajuda</a>
+            <a href="#/servico/pdf" class="btn btn-default" data-intro='Clique aqui para baixar um relatório da página atual em PDF.'>PDF</a>
+            <a href="/servico" class="btn btn-default" data-intro='Clique aqui para atualizar a página.'>Atualizar</a>
+            <a href="/servico/create" class="btn btn-default" data-intro='Clique aqui para adicionar um novo registro.'>Novo Registro</a>
         </div>
     </div>
+
+    @if(Session::has('message'))
+        <div class="alert alert-success">
+            {{ Session::get('message') }}
+        </div>
+    @endif
         
-    <table class="table table-striped table-hovered table-condesend">
+    <table class="table table-striped table-hovered table-condesend" data-intro='Aqui você confere todos os registros já cadastrados.'>
         <thead>
             <tr>
                 <th>#</th>
@@ -27,22 +33,13 @@
             <tr>
                 <td>{{$servico->cd_servico}}</td>
                 <td>{{$servico->nm_servico}}</td>
-                <td>{{$servico->vl_servico}}</td>
-                <td>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Ver <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a href="/servico/{{$servico->cd_servico}}/edit">Editar</a></li>
-                        </ul>
-                    </div>
-                </td>
+                <td>R$ {{$servico->vl_servico}}</td>
+                <td><a href="/servico/{{$servico->cd_servico}}" type="button" class="btn btn-default">Ver</a></td>
             </tr>
         @empty
             <tr>
                 <td colspan="6" class="aling-center">
-                    Não há produtos cadastrados
+                    Não há serviços cadastrados.
                 </div>
             </tr>
         @endforelse
--- resources/views/servico/show.blade.php
+++ resources/views/servico/show.blade.php
@@ -1,17 +1,18 @@
 @extends('layout/public')
 @section('content')
-    <div class="component-title">
+    <div class="component-title" data-intro='Nessa tela você poderá editar ou excluir um serviço.'>
         <h1>Detalhes do serviço</h1>
     </div>
 
     <form class="form-horizontal" action="/servico/{{$servico->cd_servico}}" method="POST">
-    {{ method_field('PUT') }}
+        {{ csrf_field() }}
+        {{ method_field('DELETE') }}
     <div class="component-barra-menu">
         <div class="btn-group pull-right" role="group">
-            <a href="/servico/help" class="btn btn-default">Ajuda</a>
-            <a href="/servico/" class="btn btn-default">Listagem</a>
-            <a href="/servico/{{$servico->cd_servico}}/edit" class="btn btn-default">Editar</a>
-            <button type="submit" class="btn btn-danger">Excluir</button>
+            <a href="#/servico/help" class="btn btn-default btn-help" data-intro='Clique aqui para ter uma ajuda igual a essa na página.'>Ajuda</a>
+            <a href="/servico/" class="btn btn-default" data-intro='Clique aqui para voltar na listagem de serviços.'>Listagem</a>
+            <a href="/servico/{{$servico->cd_servico}}/edit" class="btn btn-default" data-intro='Clique aqui para editar os dados do servico.'>Editar</a>
+            <button type="submit" class="btn btn-danger" data-intro='Clique aqui para excluir o serviço.'>Excluir</button>
         </div>
     </div>
     </form>
@@ -23,8 +24,8 @@
             <dl class="dl-horizontal">
                 <dt>Nome:</dt>
                 <dd>{{$servico->nm_servico}}</dd>
-                <dt>Descrição:</dt>
-                <dd>{{$servico->vl_servico}}</dd>
+                <dt>Valor:</dt>
+                <dd>R$ {{$servico->vl_servico}}</dd>
                 <dt>Descrição:</dt>
                 <dd>{{$servico->ds_servico}}</dd>
             </dl>
