b7b326746ad9851523475e56cf907420c619fc6b
Victor Hugo Costa <victor.hlscosta@gmail.com>
2017-06-02 23:03:42 -0300

    Ajustes na telas de conta e departamento

--- resources/views/conta/create.blade.php
+++ resources/views/conta/create.blade.php
@@ -1,12 +1,13 @@
 @extends('layout/public')
 @section('content')
-    <div class="component-title">
+    <div class="component-title" data-intro='Nessa tela você irá cadastrar uma nova conta no sistema.'>
         <h1>Cadastro de contas</h1>
     </div>
 
     <div class="component-barra-menu">
-        <div class="btn-group pull-right" role="group">
-            <a href="/usuario/help" class="btn btn-default">Ajuda</a>
+        <div class="btn-group pull-right" role="group" >
+            <a href="#/conta/help" class="btn btn-default btn-help" data-intro='Clique aqui para ter uma ajuda igual a essa na página.'>Ajuda</a>
+            <a href="/conta/" class="btn btn-default" data-intro='Clique aqui para voltar na listagem de contas.'>Listagem</a>
         </div>
     </div>
 
@@ -27,25 +28,25 @@
                     <div class="form-group">
                         <label for="nm_conta" class="col-md-4 control-label">Nome :</label>
                         <div class="col-md-6">
-                            <input type="text" class="form-control" id="nm_conta" name="nm_conta" placeholder="Nome da conta"/>
+                            <input type="text" class="form-control" id="nm_conta" name="nm_conta" placeholder="Nome da conta" required/>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="cd_agencia_conta" class="col-md-4 control-label">Agência :</label>
                         <div class="col-md-6">
-                            <input type="number" class="form-control" id="cd_agencia_conta" name="cd_agencia_conta" placeholder="Nº da agência"/>
+                            <input type="number" class="form-control" id="cd_agencia_conta" name="cd_agencia_conta" placeholder="Nº da agência" required/>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="cd_numero_conta" class="col-md-4 control-label">Conta :</label>
                         <div class="col-md-6">
-                            <input type="number" class="form-control" id="cd_numero_conta" name="cd_numero_conta" placeholder="Nº da conta"/>
+                            <input type="number" class="form-control" id="cd_numero_conta" name="cd_numero_conta" placeholder="Nº da conta" required/>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="vl_inicial_conta" class="col-md-4 control-label">Valor Inicial:</label>
                         <div class="col-md-6">
-                            <input type="text" class="form-control" id="vl_inicial_conta" name="vl_inicial_conta" placeholder="Valor inicial R$" />
+                            <input type="text" class="form-control" id="vl_inicial_conta" name="vl_inicial_conta" placeholder="Valor inicial R$" required/>
                         </div>
                     </div>
                     <div class="form-group">
