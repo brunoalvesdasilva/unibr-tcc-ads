e044c9d096d54ff98a979c397173e31e840b9004
Willian Barreto <willianblopes@gmail.com>
2017-06-04 16:19:48 -0300

    Alteracoes BD e criacao lista usuario

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    private $nameFolder = "usuario";
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $usuario = Usuario::all();
+        
+        //
+        return view("{$this->nameFolder}/list", ["listaUsuario"=>$usuario]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+		// Lista de departamento
+        $departamento = array();
+        foreach(Departamento::all() as $departamento){
+            $departamento[$departamento->cd_departamento] = $departamento->nm_departamento;
+        }
+        
+        //
+        return view("{$this->nameFolder}/create", ['departamento'=>$departamento]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Valida
+        $this->validate($request, 
+		[
+            'nm_usuario' => 'required',
+			'nm_email' => 'required',
+			'nm_senha' => 'required',
+			'cd_departamento' => 'required',
+
+        ]);
+        
+        // Adiciona e salva
+        $usuario = new Usuario();
+        $usuario->nm_usuario = $request->nm_usuario;
+        $usuario->nm_email = $request->nm_email;
+		$usuario->nm_senha = $request->nm_senha;
+		$usuario->cd_departamento = $request->cd_departamento;
+		$usuario->save();
+        
+        // Redireciona
+        return redirect('usuario')->with('message', 'Usuario salvo com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $usuario = Usuario::find($id);
+        
+        return view("{$this->nameFolder}/show", ["usuario"=>$usuario]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //Usuario
+        $usuario = Usuario::find($id);
+		
+		// Lista de departamento
+        $departamento = array();
+        foreach(Departamento::all() as $departamento){
+            $contas[$departamento->cd_departamento] = $departamento->nm_departamento;
+        }
+        
+        return view("{$this->nameFolder}/edit", ["usuario"=>$usuario, 'departamento'=>$departamento]);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Valida
+        $this->validate($request, 
+		[
+			'nm_usuario' => 'required',
+			'nm_email' => 'required',
+			'nm_senha' => 'required',
+			'cd_departamento' => 'required',
+
+        ]);
+        
+        // Adiciona e salva
+        $usuario = Usuario::find($id);
+        $usuario->nm_usuario = $request->nm_usuario;
+		$usuario->nm_email = $request->nm_email;
+		$usuario->nm_senha = $request->nm_senha;
+		$usuario->cd_departamento = $request->cd_departamento;
+        $usuario->save();
+        
+        // Redireciona
+        return redirect("usuario/$id")->with('message', 'Usuario editado com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // Adiciona e salva
+        $usuario = Usuario::find($id);
+        $usuario->delete();
+        
+        // Redireciona
+        return redirect('usuario')->with('message', 'Usuario deletado com sucesso!');
+    }
+}
