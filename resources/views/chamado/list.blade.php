74d5c5e50909ea24ee40e792b5956867b38ae1bd
Willian Barreto <willianblopes@gmail.com>
2017-06-09 10:51:14 -0300

    Atualizando modulo Chamado

--- resources/views/chamado/list.blade.php
+++ resources/views/chamado/list.blade.php
@@ -1,58 +1,40 @@
 @extends('layout/public')
 @section('content')
-    <div class="component-title">
+ <div class="component-title" data-intro='Nessa tela você irá manusear as informações dos chamados.'>
         <h1>Listagem de chamados</h1>
     </div>
         
     <div class="component-barra-menu">
         <div class="btn-group pull-right" role="group">
-            <a href="/chamado/help" class="btn btn-default">Ajuda</a>
-            <a href="/chamado/pdf" class="btn btn-default">PDF</a>
-            <a href="/chamado" class="btn btn-default">Atualizar</a>
-            <a href="/chamado/create" class="btn btn-default">Novo Registro</a>
+            <a href="#/chamado/help" class="btn btn-default btn-help" data-intro='Clique aqui para ter uma ajuda igual a essa na página.'>Ajuda</a>
+            <a href="#/chamado/pdf" class="btn btn-default" data-intro='Clique aqui para baixar um relatório da página atual em PDF.'>PDF</a>
+            <a href="/chamado" class="btn btn-default" data-intro='Clique aqui para atualizar a página.'>Atualizar</a>
+            <a href="/chamado/create" class="btn btn-default" data-intro='Clique aqui para adicionar um novo registro.'>Novo Registro</a>
         </div>
     </div>
         
-    <table class="table table-striped table-hovered table-condesend">
+    <table class="table table-striped table-hovered table-condesend" data-intro='Aqui você confere todos os registros já cadastrados.'>
         <thead>
             <tr>
                 <th>#</th>
-                <th>Data abertura</th>
-                <th>Descrição do chamado</th>
+                <th>Chamado</th>
                 <th>Ação</th>
             </tr>
         </thead>
         <tbody>
+        @forelse($listaChamado as $chamado)
             <tr>
-                <td>#</td>
-                <td>Data abertura</td>
-                <td>Descrição chamado</td>
-                <td>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Ver <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Excluir</a></li>
-                        </ul>
-                    </div>
-                </td>
+                <td>{{$chamado->cd_chamado}}</td>
+                <td>{{$chamado->ds_chamado}}</td>
+                <td><a href="/chamado/{{$chamado->cd_chamado}}" type="button" class="btn btn-default">Ver</a></td>
             </tr>
+        @empty
             <tr>
-                <td>#</td>
-                <td>Nome</td>
-                <td>Data de cadastro</td>
-                <td>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Ver <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Excluir</a></li>
-                        </ul>
+                <td colspan="6" class="aling-center"/>
+                    Não há chamados cadastrados
                     </div>
-                </td>
             </tr>
-        </tbody>
+        @endforelse
+        </tbody>           
     </table>
 @stop
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::resource('conta', 'ContaController');
 Route::resource('usuario', 'UsuarioController');
 Route::resource('movimentacao', 'MovimentacaoController');
 Route::resource('usuario','UsuarioController');
+Route::resource('chamado','ChamadoController');
 
 Route::get('/icomoon', function () {
     return view('fonts');
