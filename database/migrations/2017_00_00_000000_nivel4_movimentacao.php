2b71a790c649e401e666c48fc883ff9219505fed
Bruno Alves da Silva <contato@alvesbruno.com>
2017-06-09 14:21:22 -0300

    Correção da migration

--- database/migrations/2017_00_00_000000_nivel4_movimentacao.php
+++ database/migrations/2017_00_00_000000_nivel4_movimentacao.php
@@ -17,21 +17,22 @@ class Nivel4Movimentacao extends Migration
         Schema::create('movimentacao', function (Blueprint $table) {
             
             // Básico
-            $table->increments('cd_movimentacao')->comment('Campo auto-incremento,não nulo,maior que zero,chave primaria');
+            $table->increments('cd_movimentacao')->comment('Campo auto incremento, não nulo, maior que zero, chave primaria');
             $table->string('nm_movimentacao', 50)->comment('Nome da Movimentação');
-            $table->enum('ic_tipo_movimentacao', ['receita', 'despesa'])->comment('Indicador tipo da movimentacao');
+            $table->enum('ic_tipo_movimentacao', ['debito', 'credito'])->comment('Indicador tipo da movimentação');
             $table->date('dt_movimentacao')->comment('Data da Movimentação');
-            $table->integer('cd_nf_movimentacao')->comment('Codigo nota fiscal da movimentacao');
-            $table->dateTime('dt_registro_movimentacao')->comment('Data do registro da movimentacao');
-            $table->enum('ic_pago_sim_nao', ['sim', 'nao'])->comment('Indicador movimentacao paga, sim-nao');
-            $table->float('vl_movimentacao', 8,2)->comment('Vslor da movimentacao');
-            $table->string('ds_movimentacao',200)->nullable()->comment('Descrição da movimentacao');
-            $table->enum('ic_recorrente_sim_nao', ['sim', 'nao'])->comment('Indicador movimentacao recorrente, sim-nao');
+
+            $table->integer('cd_nf_movimentacao')->comment('Código nota fiscal da movimentação');
+            $table->dateTime('dt_registro_movimentacao')->comment('Data do registro da movimentação');
+            $table->enum('ic_pago_sim_nao', ['sim', 'nao'])->comment('Indicador movimentação paga, sim-nao');
+            $table->float('vl_movimentacao', 8,2)->comment('Valor da movimentação');
+            $table->string('ds_movimentacao',200)->nullable()->comment('Descrição da movimentação');
+            $table->enum('ic_recorrente_sim_nao', ['sim', 'nao'])->comment('Indicador movimentação recorrente, sim-nao');
             
             // Chaves estrangeiras
-            $table->integer('cd_conta')->unsigned()->comment('Campo auto-incremento,não nulo,maior que zero,chave estrangeira, Tabela:Conta/cd_conta');
+            $table->integer('cd_conta')->unsigned()->comment('Campo não nulo, maior que zero, chave estrangeira, Tabela:Conta/cd_conta');
             $table->foreign('cd_conta')->references('cd_conta')->on('conta');
-            $table->integer('cd_contrato')->unsigned()->nullable()->comment('Campo auto-incremento,não nulo,maior que zero,chave estrangeira, Tabela:Contrato/cd_contrato');
+            $table->integer('cd_contrato')->unsigned()->nullable()->comment('Campo não nulo, maior que zero, chave estrangeira, Tabela:Contrato/cd_contrato');
             $table->foreign('cd_contrato')->references('cd_contrato')->on('contrato');
             
             // Defaults
