197a5889b77ea3d6194d06456ce1ef0032ff96a4
Willian Barreto <willianblopes@gmail.com>
2017-06-29 10:12:00 -0300

    Versao

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,10 @@ Route::resource('produto', 'ProdutoController');
 Route::resource('departamento', 'DepartamentoController');
 Route::resource('servico', 'ServicoController');
 Route::resource('conta', 'ContaController');
-Route::resource('usuario', 'UsuarioController');
 Route::resource('movimentacao', 'MovimentacaoController');
 Route::resource('usuario','UsuarioController');
 Route::resource('chamado','ChamadoController');
-});
+//});
     
 Route::get('/icomoon', function () {
     return view('fonts');
